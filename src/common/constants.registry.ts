81fbba8a40320b1bd4ae8fba19ffacd1b95e8744
jhoyosR <jhoyos0410@cue.edu.co>
2026-06-09 14:41:42 -0500

    New: Agrega endpoint para obtener datos de tipo enum en selectores

---
+++ src/common/constants.registry.ts
@@ -0,0 +1,11 @@
+import { IndustryType } from "../company/enum/industry-type.enum";
+import { Career } from "../student/enum/career.enum";
+import { Modality } from "../vacancie/enum/modality.enum";
+import { VacancieStatus } from "../vacancie/enum/vacancie-status.enum";
+
+export const CONSTANTS_REGISTRY: Record<string, object> = {
+  'career': Career,
+  'industry-type': IndustryType,
+  'modality': Modality,
+  'vacancie-status': VacancieStatus
+};
